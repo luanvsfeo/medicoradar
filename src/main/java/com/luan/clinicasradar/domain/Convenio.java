2fc2ce5eb8c582690df2b3ebc5ccee91a1b4c1d8
luanfeo <luanfeo@gmail.com>
2020-04-21 15:46:42 -0300

    master - Inclusão das anotations do JPA

--- src/main/java/com/luan/clinicasradar/domain/Convenio.java
+++ src/main/java/com/luan/clinicasradar/domain/Convenio.java
@@ -1,4 +1,206 @@
 package com.luan.clinicasradar.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name="convenios")
 public class Convenio {
+
+    @Id
+    private String registroAns;
+    private String cnpj;
+    private String razaoSocial;
+    private String nomeFantasia;
+    private String modalidade;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    @Column(nullable = true)
+    private Integer cep;
+    @Column(nullable = true)
+    private Integer ddd;
+    @Column(nullable = true)
+    private String telefone;
+    @Column(nullable = true)
+    private String fax;
+    @Column(nullable = true)
+    private String enderecoEletronico;
+    @Column(nullable = true)
+    private String representante;
+    @Column(nullable = true)
+    private String cargoRepresentante;
+    @Column(nullable = true)
+    private Integer regiaoAtuacao;
+    @Column(nullable = true)
+    private Date dataRegistroAns;
+
+    public String getRegistroAns() {
+        return registroAns;
+    }
+
+    public void setRegistroAns(String registroAns) {
+        this.registroAns = registroAns;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getModalidade() {
+        return modalidade;
+    }
+
+    public void setModalidade(String modalidade) {
+        this.modalidade = modalidade;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+
+    public Integer getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(Integer ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEnderecoEletronico() {
+        return enderecoEletronico;
+    }
+
+    public void setEnderecoEletronico(String enderecoEletronico) {
+        this.enderecoEletronico = enderecoEletronico;
+    }
+
+    public String getRepresentante() {
+        return representante;
+    }
+
+    public void setRepresentante(String representante) {
+        this.representante = representante;
+    }
+
+    public String getCargoRepresentante() {
+        return cargoRepresentante;
+    }
+
+    public void setCargoRepresentante(String cargoRepresentante) {
+        this.cargoRepresentante = cargoRepresentante;
+    }
+
+    public Integer getRegiaoAtuacao() {
+        return regiaoAtuacao;
+    }
+
+    public void setRegiaoAtuacao(Integer regiaoAtuacao) {
+        this.regiaoAtuacao = regiaoAtuacao;
+    }
+
+    public Date getDataRegistroAns() {
+        return dataRegistroAns;
+    }
+
+    public void setDataRegistroAns(Date dataRegistroAns) {
+        this.dataRegistroAns = dataRegistroAns;
+    }
 }
+
+
+
--- src/main/java/com/luan/clinicasradar/domain/Plano.java
+++ src/main/java/com/luan/clinicasradar/domain/Plano.java
@@ -1,4 +1,15 @@
 package com.luan.clinicasradar.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="planos")
 public class Plano {
+    @Id
+    private String id;
+    private String codigoPlano;
+    private String nomePlano;
+
 }
