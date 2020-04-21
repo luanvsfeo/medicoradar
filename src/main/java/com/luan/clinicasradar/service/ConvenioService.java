f90b4430f19dbcac715eab5d73cd9c101a74e545
luanfeo <luanfeo@gmail.com>
2020-04-21 15:51:20 -0300

    master - Inclusão do atributo referente as respectivas "repository"

--- src/main/java/com/luan/clinicasradar/service/ConvenioService.java
+++ src/main/java/com/luan/clinicasradar/service/ConvenioService.java
@@ -1,7 +1,21 @@
 package com.luan.clinicasradar.service;
 
+import com.luan.clinicasradar.domain.Convenio;
+import com.luan.clinicasradar.repository.ConvenioRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class ConvenioService {
+
+    private ConvenioRepository convenioRepository;
+
+    public ConvenioService(ConvenioRepository convenioRepository) {
+        this.convenioRepository = convenioRepository;
+    }
+
+    public Collection<Convenio> buscarTodosConvenios(){
+        return convenioRepository.findAll();
+    }
 }
--- src/main/java/com/luan/clinicasradar/service/PlanoService.java
+++ src/main/java/com/luan/clinicasradar/service/PlanoService.java
@@ -1,12 +1,23 @@
 package com.luan.clinicasradar.service;
 
+import com.luan.clinicasradar.repository.PlanoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlanoService {
 
+   private final  PlanoRepository planoRepository;
+
+
+    public PlanoService(PlanoRepository planoRepository) {
+        this.planoRepository = planoRepository;
+    }
 
     public String buscarPlanos(){
         return "puta que pariu";
+    }
+
+    public String buscarPlanosPorId(int convenioId){
+        return null;
     }
 }
