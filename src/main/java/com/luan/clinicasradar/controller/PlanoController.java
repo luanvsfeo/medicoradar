1e866c255c9a39c767f079018c99b383717c3bf5
luanfeo <luanfeo@gmail.com>
2020-04-25 17:19:26 -0300

    master - Alteração do retorno da função e inclusão da chamada da service

--- src/main/java/com/luan/clinicasradar/controller/PlanoController.java
+++ src/main/java/com/luan/clinicasradar/controller/PlanoController.java
@@ -1,12 +1,13 @@
 package com.luan.clinicasradar.controller;
 
+import com.luan.clinicasradar.domain.Plano;
 import com.luan.clinicasradar.service.PlanoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/plano")
@@ -21,14 +22,13 @@ public class PlanoController {
     }
 
     @GetMapping("/")
-    public String getAllPlanos(){
-       String a =  planoService.buscarPlanos();
-        return a;
+    public Collection<Plano> getAllPlanos(){
+        return planoService.buscarPlanos();
     }
 
     @GetMapping("/convenio/{conveioId}")
-    public String getAllPlanosByConvenioId(@PathParam("conveioId") int conveioId){
-        return "";
+    public Collection<Plano> getAllPlanosByConvenioId(@PathParam("conveioId") String conveioId){
+        return planoService.buscarPlanosPorId(conveioId);
     }
 
 
