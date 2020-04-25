90b655ccdf1ad370f835a70291e05ff214b187f0
luanfeo <luanfeo@gmail.com>
2020-04-25 17:18:23 -0300

    master - Inclusão da função para busca de plano pelo codigo da operadora/convenio

--- src/main/java/com/luan/clinicasradar/repository/PlanoRepository.java
+++ src/main/java/com/luan/clinicasradar/repository/PlanoRepository.java
@@ -2,8 +2,14 @@ package com.luan.clinicasradar.repository;
 
 import com.luan.clinicasradar.domain.Plano;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface PlanoRepository extends JpaRepository<Plano,String> {
+
+    @Query(name = "",nativeQuery = true)
+    Collection<Plano> findPlanosByCdOperadora(String cdOperacora);
 }
