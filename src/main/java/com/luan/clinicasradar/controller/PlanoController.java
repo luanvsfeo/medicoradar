6ad1d751d89bf01c3201cee7b9919cbbaaaba6eb
luanfeo <luanfeo@gmail.com>
2020-04-26 14:21:19 -0300

    master - Troca de PathParam por PathVariable

--- src/main/java/com/luan/clinicasradar/controller/PlanoController.java
+++ src/main/java/com/luan/clinicasradar/controller/PlanoController.java
@@ -3,6 +3,7 @@ package com.luan.clinicasradar.controller;
 import com.luan.clinicasradar.domain.Plano;
 import com.luan.clinicasradar.service.PlanoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,7 @@ public class PlanoController {
     }
 
     @GetMapping("/convenio/{conveioId}")
-    public Collection<Plano> getAllPlanosByConvenioId(@PathParam("conveioId") String conveioId){
+    public Collection<Plano> getAllPlanosByConvenioId(@PathVariable("conveioId") String conveioId){
         return planoService.buscarPlanosPorId(conveioId);
     }
 
