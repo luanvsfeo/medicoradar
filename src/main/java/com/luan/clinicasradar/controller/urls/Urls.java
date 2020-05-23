25fbd2f6e80935b4b16a91a61008b9986111f494
luanfeo <luanfeo@gmail.com>
2020-05-23 13:48:47 -0300

    master - Inclusão das urls em um arquivo separado

---
+++ src/main/java/com/luan/clinicasradar/controller/urls/Urls.java
@@ -0,0 +1,11 @@
+package com.luan.clinicasradar.controller.urls;
+
+
+public class Urls {
+
+
+    public static final String CONVENIO = "/convenio";
+    public static final String PLANOS_POR_CONVENIO = "/convenio/{convenioId}/planos";
+    public static final String ESTABELECIMENTOS_POR_PLANO_CONVENIO = "/estabelecimento/{cdOperadora}/{cdPlano}";
+
+}
