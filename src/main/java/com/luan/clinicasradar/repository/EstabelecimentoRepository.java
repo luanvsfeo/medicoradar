1bae754b62202862ae2b06f044e6133607457e04
luanfeo <luanfeo@gmail.com>
2020-05-23 18:45:27 -0300

    master - Refatoração da query presente na função de busca de Estabelecimento

--- src/main/java/com/luan/clinicasradar/repository/EstabelecimentoRepository.java
+++ src/main/java/com/luan/clinicasradar/repository/EstabelecimentoRepository.java
@@ -3,6 +3,7 @@ package com.luan.clinicasradar.repository;
 import com.luan.clinicasradar.domain.Estabelecimento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -10,6 +11,6 @@ import java.util.Collection;
 @Repository
 public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento,String> {
 
-    @Query(value = " SELECT * FROM estabelecimento",nativeQuery = true)
-    Collection<Estabelecimento> buscarEstabelecimentosPorCdPLanoECdOperadora();
+    @Query(value = " SELECT * FROM estabelecimento where cd_operadora  = :cd_operadora AND cd_plano = :cd_plano",nativeQuery = true)
+    Collection<Estabelecimento> buscarEstabelecimentosPorCdPLanoECdOperadora(@Param("cd_operadora") String cdOperadora, @Param("cd_plano") String cdPlano);
 }
