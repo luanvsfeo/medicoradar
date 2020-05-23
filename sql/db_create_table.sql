bebff2323b4d4c5f3e5ce452c4898a0a038561a6
luanfeo <luanfeo@gmail.com>
2020-05-23 18:50:24 -0300

    master - salve de ddl das tabelas

--- sql/db_create_table.sql
+++ sql/db_create_table.sql
@@ -113,3 +113,94 @@ create table estabelecimento(
 	NM_REGIAO                       varchar(255) null,
 	constraint pk_estabelecimento primary key (ID_REDE)
 );
+
+
+
+
+---------------------------- comparar com os acima -------------------------
+CREATE TABLE convenios(
+  Registro_ANS        varchar(240)   NULL ,
+  CNPJ                varchar(240)   null,
+  Razao_Social        VARCHAR(240)  null,
+  Nome_Fantasia       VARCHAR(240),
+  Modalidade          VARCHAR(280)  null,
+  Logradouro          VARCHAR(240)  null,
+  Numero              VARCHAR(230)  null,
+  Complemento         VARCHAR(240),
+  Bairro              VARCHAR(240),
+  Cidade              VARCHAR(230)  null,
+  UF                  VARCHAR(23)  null,
+  CEP                	integer    null,
+  DDD                 integer ,
+  Telefone            VARCHAR(220),
+  Fax                 VARCHAR(220),
+  Endereco_eletronico VARCHAR(255),
+  Representante       VARCHAR(250)  null,
+  Cargo_Representante VARCHAR(240),
+  Regiao_Atuacao   INTEGER null,
+  Data_Registro_ANS   DATE   null,
+  constraint pk_convenios primary key (Registro_ANS)
+);
+
+create table planos_convenios(
+	PLANO_DT varchar(255) null,
+	ID_NOTA varchar(255) not null,
+	CD_OPERADORA varchar(255) null,
+	ID_PLANO varchar(255) null,
+	CD_PLANO varchar(255) null,
+	CD_NOTA varchar(255) null,
+	DT_NTRP date null,
+	ID_ABRG varchar(255) null,
+	NM_ARQV varchar(255) null,
+	NO_RAZAO varchar(255) null,
+	CD_GRUPO_CLASSIFICACAO varchar(255) null,
+	DS_CLASSIFICACAO varchar(255) null,
+	IN_SITUACAO varchar(255) null,
+	COBERTURA varchar(255) null,
+	TP_VIGENCIA_PLANO varchar(255) null,
+	SGMT_ASSISTEN varchar(255) null,
+	ID_TIPO_SEGMENTACAO varchar(255) null,
+	DE_TIPO_SEGMENTACAO varchar(255) null,
+	ID_TIPO_CONTRATACAO varchar(255) null,
+	DE_TIPO_CONTRATACAO varchar(255) null,
+	DE_TIPO_MODALIDADE_FINM varchar(255) null,
+	ID_TIPO_ABRANGENCIA varchar(255) null,
+	DE_TIPO_ABRANGENCIA_GEOGRAFICA varchar(255) null,
+	LG_FATOR_MODERADOR varchar(255) null,
+	DE_FATOR_MODERADOR varchar(255) null,
+	ACOMODACAO varchar(255) null,
+	constraint pk_planos_convenios primary key(ID_NOTA)
+);
+
+create table estabelecimento(
+	ID_REDE  varchar(255) null,
+	CD_OPERADORA varchar(255) null,
+	NM_PRAZO varchar(255) null,
+	DS_CLASSIFICACAO varchar(255) null,
+	DE_PORTE varchar(255) null,
+	ID_PLANO varchar(255) null,
+	CD_PLANO varchar(255) null,
+	CONTRATACAO varchar(255) null,
+	DE_TIPO_CONTRATACAO varchar(255) null,
+	DE_TIPO_MODALIDADE_FINM varchar(255) null,
+	SEGMENTACAO_ASSISTENCIAL varchar(255) null,
+	DE_TIPO_ABRANGENCIA_GEOGRAFICA varchar(255) null,
+	LG_FATOR_MODERADOR varchar(255) null,
+	DE_SITUACAO_PRINCIPAL varchar(255) null,
+	ID_ESTABELECIMENTO_SAUDE varchar(255) null,
+	CD_CNPJ_ESTB_SAUDE varchar(255) null,
+	CD_CNES varchar(255) null,
+	NM_ESTABELECIMENTO_SAUDE varchar(255) null,
+	DE_CLAS_ESTB_SAUDE varchar(255) null,
+	LG_URGENCIA_EMERGENCIA varchar(255) null,
+	DE_TIPO_PRESTADOR varchar(255) null,
+	DE_TIPO_CONTRATO varchar(255) null,
+	DE_DISPONIBILIDADE varchar(255) null,
+	CD_MUNICIPIO varchar(255) null,
+	NM_MUNICIPIO_X varchar(255) null,
+	SG_UF varchar(255) null,
+	DT_VINCULO_INICIO varchar(255) null,
+	DT_VINCULO_FIM varchar(255) null,
+	NM_REGIAO varchar(255) null,
+	constraint pk_estabelecimento primary key (ID_REDE)
+);
