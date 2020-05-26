09a89f849dd4f8fbe9aba3aec45f50e6a03ae99a
luanfeo <luanfeo@gmail.com>
2020-05-25 22:47:35 -0300

    dev - Inclusão da annotation que permiti utilizar JOB na aplicação

--- src/main/java/com/luan/clinicasradar/ClinicasradarApplication.java
+++ src/main/java/com/luan/clinicasradar/ClinicasradarApplication.java
@@ -2,9 +2,11 @@ package com.luan.clinicasradar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication(scanBasePackages={"com.luan.clinicasradar"})
+@EnableScheduling
 public class ClinicasradarApplication {
 
 	public static void main(String[] args) {
