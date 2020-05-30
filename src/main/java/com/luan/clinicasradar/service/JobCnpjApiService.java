2a2bccbee6125045920a1f76ed1e3e6c217b3e25
luanfeo <luanfeo@gmail.com>
2020-05-30 14:29:24 -0300

    dev - Desenvolvimento de uma parte do job de atualização dos estabelecimentos

--- src/main/java/com/luan/clinicasradar/service/JobCnpjApiService.java
+++ src/main/java/com/luan/clinicasradar/service/JobCnpjApiService.java
@@ -1,6 +1,7 @@
 package com.luan.clinicasradar.service;
 
 import com.luan.clinicasradar.domain.EstabelecimentoApi;
+import com.luan.clinicasradar.domain.EstabelecimentoInfo;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -9,16 +10,25 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class JobCnpjApiService {
 
+    private EstabelecimentoInfoService estabelecimentoInfoService;
+
+    public JobCnpjApiService(EstabelecimentoInfoService estabelecimentoInfoService) {
+        this.estabelecimentoInfoService = estabelecimentoInfoService;
+    }
 
     @Scheduled(cron="0 0/1 * 1/1 * ?")
     public void buscarInformacoesNaApi(){
-         //RestTemplate restTemplate = new RestTemplate();
 
-        //String url = "https://www.receitaws.com.br/v1/cnpj/45615309000124";
-        //String url = "https://www.receitaws.com.br/v1/cnpj/{cnpjClinica}";
-        //EstabelecimentoApi estabelecimentoApi = restTemplate.getForObject(url, EstabelecimentoApi.class);
+        EstabelecimentoInfo estabelecimento = estabelecimentoInfoService.buscarEstabelecimentoAntesDoJob();
 
-        // TODO - Colocar para buscar todas os estabelecimentos que não possuem o campo logradouro
+         RestTemplate restTemplate = new RestTemplate();
+
+         String url = "https://www.receitaws.com.br/v1/cnpj/{cnpjClinica}";
+        EstabelecimentoApi estabelecimentoApi = restTemplate.getForObject(url, EstabelecimentoApi.class,estabelecimento.getCnpj());
+        estabelecimento.atualizar(estabelecimentoApi);
+        estabelecimentoInfoService.atualizarAposJob(estabelecimento);
+        System.out.println("Estabelecimento atualizado - CNPJ:" + estabelecimento.getCnpj());
+
         // TODO - Criar job para converter cep em latitude e longitude
 
 
