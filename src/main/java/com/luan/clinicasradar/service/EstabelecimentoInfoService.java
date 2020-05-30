d2bb52422e53eb85faa1f005ce8c5ee1d3e8e0eb
luanfeo <luanfeo@gmail.com>
2020-05-30 14:28:20 -0300

    dev - Inclusão da classe EstabelecimentoInfoService

---
+++ src/main/java/com/luan/clinicasradar/service/EstabelecimentoInfoService.java
@@ -0,0 +1,29 @@
+package com.luan.clinicasradar.service;
+
+import com.luan.clinicasradar.domain.EstabelecimentoInfo;
+import com.luan.clinicasradar.repository.EstabelecimentoInfoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class EstabelecimentoInfoService {
+
+    private EstabelecimentoInfoRepository estabelecimentoInfoRepository;
+
+    public EstabelecimentoInfoService(EstabelecimentoInfoRepository estabelecimentoInfoRepository) {
+        this.estabelecimentoInfoRepository = estabelecimentoInfoRepository;
+    }
+
+    public Collection<EstabelecimentoInfo> buscarTodas(){
+        return estabelecimentoInfoRepository.findAll();
+    }
+
+    public EstabelecimentoInfo buscarEstabelecimentoAntesDoJob(){
+        return estabelecimentoInfoRepository.findOneBeforeJob();
+    }
+
+    public void atualizarAposJob(EstabelecimentoInfo estabelecimentoInfo){
+        estabelecimentoInfoRepository.save(estabelecimentoInfo);
+    }
+}
