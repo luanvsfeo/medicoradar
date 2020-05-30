86ce29ef22dd2793f9b31bd3dc8df5b88b6e5505
luanfeo <luanfeo@gmail.com>
2020-05-30 14:28:36 -0300

    dev - Inclusão da classe EstabelecimentoInfoRepository

---
+++ src/main/java/com/luan/clinicasradar/repository/EstabelecimentoInfoRepository.java
@@ -0,0 +1,14 @@
+package com.luan.clinicasradar.repository;
+
+import com.luan.clinicasradar.domain.EstabelecimentoInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstabelecimentoInfoRepository extends JpaRepository<EstabelecimentoInfo, String> {
+
+    @Query(value = "select * from estabelecimento_info where logradouro is null limit 1",nativeQuery = true)
+    EstabelecimentoInfo findOneBeforeJob();
+
+}
