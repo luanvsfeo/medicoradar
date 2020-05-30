2210738ea260c229121e6766373e0cb916cf866c
luanfeo <luanfeo@gmail.com>
2020-05-30 15:46:25 -0300

    dev - Inclusão do metodo findAllAfterJobByCdOperadoraAndCdPlano

--- src/main/java/com/luan/clinicasradar/repository/EstabelecimentoInfoRepository.java
+++ src/main/java/com/luan/clinicasradar/repository/EstabelecimentoInfoRepository.java
@@ -3,7 +3,10 @@ package com.luan.clinicasradar.repository;
 import com.luan.clinicasradar.domain.EstabelecimentoInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface EstabelecimentoInfoRepository extends JpaRepository<EstabelecimentoInfo, String> {
@@ -11,4 +14,6 @@ public interface EstabelecimentoInfoRepository extends JpaRepository<Estabelecim
     @Query(value = "select * from estabelecimento_info where situacao is null limit 1",nativeQuery = true)
     EstabelecimentoInfo findOneBeforeJob();
 
+    @Query(value = "select * from estabelecimento_info where cnpj in (SELECT cd_cnpj_estb_saude FROM estabelecimento where cd_plano = :cd_plano and cd_operadora = :cd_operadora)",nativeQuery = true)
+    Collection<EstabelecimentoInfo> findAllAfterJobByCdOperadoraAndCdPlano(@Param("cd_operadora") String cdOperadora, @Param("cd_plano") String cdPlano);
 }
