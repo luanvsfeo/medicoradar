478445476eebfcb6735bf03fa56781fc439a45ca
luanfeo <luanfeo@gmail.com>
2020-06-02 23:05:02 -0300

    dev - cors filter

--- src/main/java/com/luan/clinicasradar/ClinicasradarApplication.java
+++ src/main/java/com/luan/clinicasradar/ClinicasradarApplication.java
@@ -2,7 +2,10 @@ package com.luan.clinicasradar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication(scanBasePackages={"com.luan.clinicasradar"})
@@ -13,4 +16,13 @@ public class ClinicasradarApplication {
 		SpringApplication.run(ClinicasradarApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
