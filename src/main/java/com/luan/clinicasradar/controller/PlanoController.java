7f42d029d5a431f12041331f22275fb8810e69b7
luanfeo <luanfeo@gmail.com>
2020-06-06 15:59:40 -0300

    dev - Endpoint temporario

--- src/main/java/com/luan/clinicasradar/controller/PlanoController.java
+++ src/main/java/com/luan/clinicasradar/controller/PlanoController.java
@@ -5,10 +5,8 @@ import com.luan.clinicasradar.domain.Plano;
 import com.luan.clinicasradar.service.PlanoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
 import java.util.Collection;
 
 @RestController
@@ -25,6 +23,19 @@ public class PlanoController {
     @GetMapping(Urls.PLANOS_POR_CONVENIO)
     public Collection<Plano> getAllPlanosByConvenioId(@PathVariable("convenioId") String convenioId){
         return planoService.buscarPlanosPorId(convenioId);
+    }
+
+
+    @GetMapping("planos/atualizar")
+    public void atualizarInformacaoQuebrada(){
+
+        Collection<Plano> planos = planoService.buscarPlanos();
+
+        for ( Plano plano : planos){
+            plano.atualizarNome();
+        }
+        planoService.salvarTodas(planos);
+
     }
 
 }
